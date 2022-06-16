5d0e13709ce7c42fb5f0f0639c8d6a7e60ff40e9
RT0913 <foresta2mare@gmail.com>
2022-06-17 01:59:17 +0900

    Rspec: application_helper_specの修正

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
   describe "full_titleのデータ取得テスト" do
     context "page_titleがemptyの場合" do
-      it "連絡帳を返す" do
-        expect(full_title("")).to eq("連絡帳")
+      it "GET&READYを返す" do
+        expect(full_title("")).to eq("GET&READY")
       end
     end
 
     context "page_titleに引数を含む場合" do
-      it "title - 連絡帳を返す" do
-        expect(full_title("title")).to eq("title - 連絡帳")
+      it "title - GET&READYを返す" do
+        expect(full_title("title")).to eq("title - GET&READY")
       end
     end
 
     context "page_titleがnilの場合" do
-      it "連絡帳を返す" do
-        expect(full_title(nil)).to eq("連絡帳")
+      it "GET&READYを返す" do
+        expect(full_title(nil)).to eq("GET&READY")
       end
     end
   end
